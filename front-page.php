b577c9f88b89d6b8632b483dad1075903bce899c
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-05 06:30:09 +0900

    [update]topページのWordPress化

--- front-page.php
+++ front-page.php
@@ -1,234 +1,152 @@
 <?php
-
-/**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * sumigoya
- */
-
 get_header();
 ?>
+    <section class="main-visual" id="js-main-visual"><img class="main-visual__logo" src="/img/common/logo.png" alt="">
+      <div class="main-visual__text-box">
+        <h2 class="main-visual__title">チャーガとは</h2>
+        <p class="main-visual__sub-title">About chaga</p>
+        <p class="main-visual__lead">和名はカバノアナタケ。<br>｢森の宝石｣とも呼ばれる<br>白樺の幹に寄生するキノコのことです。</p>
+        <div class="main-visual__img-box u-hidden-o-md"><img src="/img/top/img-mainvisual.jpg" alt=""></div>
+        <p class="main-visual__text">導入テキストが入ります。和名はカバノアナタケ。｢森の宝石｣とも呼ばれる白樺の幹に寄生するキノコのことです。導入テキストが入ります。和名はカバノアナタケ。｢森の宝石｣とも呼ばれる白樺の幹に寄生するキノコのことです。導入テキストが入ります。</p><a class="btn-type1" href="/chaga">チャーガについて
+          <div class="arrow"></div></a>
+      </div>
+    </section>
+    <div class="fixed-link" id="js-fixed-link"><a class="fixed-link__shopify" href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">購入する</a><a class="fixed-link__instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">Follow us<img src="/img/common/icon-instagram.svg" alt=""></a></div>
+    <div class="contents-wrapper" id="js-contents-wrapper">
+      <div class="item-slider">
+        <div class="item-slider__item"><img src="/img/top/img-slider1.jpg" alt=""></div>
+        <div class="item-slider__item"><img src="/img/top/img-slider2.jpg" alt=""></div>
+        <div class="item-slider__item"><img src="/img/top/img-slider3.jpg" alt=""></div>
+        <div class="item-slider__item"><img src="/img/top/img-slider1.jpg" alt=""></div>
+        <div class="item-slider__item"><img src="/img/top/img-slider2.jpg" alt=""></div>
+        <div class="item-slider__item"><img src="/img/top/img-slider3.jpg" alt=""></div>
+      </div><a class="btn-type1 u-mt-60-xs u-mt-80-lg" href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">商品一覧へ
+        <div class="arrow"></div></a>
+      <section class="section-efficacy">
+        <h2 class="section-efficacy__title">チャーガに含まれる豊富な栄養成分が、<br>あなたの日々の健康維持をサポートいたします。</h2>
+        <div class="section-efficacy__contents">
+          <div class="section-efficacy__contents-wrapper">
+            <ul class="section-efficacy__ingredient">
+              <li class="js-efficacy-toggle is-active" id="ingredient-0">B-グルカン</li>
+              <li class="js-efficacy-toggle" id="ingredient-1">ポリフェノール</li>
+              <li class="js-efficacy-toggle" id="ingredient-2">マグネシウム</li>
+              <li class="js-efficacy-toggle" id="ingredient-3">サポニン</li>
+              <li class="js-efficacy-toggle" id="ingredient-4">エルゴステロール</li>
+              <li class="js-efficacy-toggle" id="ingredient-5">ベツリン酸</li>
+              <li class="js-efficacy-toggle" id="ingredient-6">マンガン</li>
+              <li class="js-efficacy-toggle" id="ingredient-7">カルシウム</li>
+              <li class="js-efficacy-toggle" id="ingredient-8">ナトリウム</li>
+              <li class="js-efficacy-toggle" id="ingredient-9">フラボノイド</li>
+              <li class="js-efficacy-toggle" id="ingredient-10">イノシトール</li>
+              <li class="js-efficacy-toggle" id="ingredient-11">リグニン</li>
+              <li class="js-efficacy-toggle u-hidden-u-lg" id="ingredient-12">メラニン</li>
+            </ul>
+            <div class="section-efficacy__description">
+              <div class="section-efficacy__description-next u-hidden-u-lg" id="js-efficacy-next-btn"></div>
+              <div class="section-efficacy__description-prev u-hidden-u-lg" id="js-efficacy-prev-btn"></div>
+              <p id="js-efficacy-description">チャーガの栄養素の働きについてのテキストを入れられます。上記の各要素をタップするとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４るとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４るとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４</p>
+            </div><img class="section-efficacy__img" src="/img/top/img-efficacy.png" alt="">
+          </div>
+        </div>
+      </section>
+      <section class="section-belief">
+        <div class="section-belief__lead">
+          <div class="section-belief__lead-wrapper">
+            <h2 class="section-belief__title">チャーガ専門ブランド<br>紫翠のこだわり</h2>
+            <p class="section-belief__contents-text section-belief__sub-title">Chaga specialty brand Shisui’s philosophy</p><img src="/img/top/img-belief-sp.jpg" alt="">
+            <p class="section-belief__contents-text section-belief__text">“みなさまの身体と人生が、<br>美しくみずみずしくありますように”<br>という私たちの健康に対する想いを込めました。</p>
+          </div>
+        </div>
+        <div class="section-belief__contents">
+          <div class="accordion js-accordion u-hidden-o-md">
+            <div class="accordion__item js-accordion-trigger">
+              <div class="accordion__title-box">
+                <h3 class="accordion__title section-belief__contents-title">01 ロシア産最高級チャーガ</h3>
+              </div>
+              <div class="accordion__content"><img src="/img/top/img-belief-accordion-1.jpg" alt="">
+                <p class="section-belief__contents-text">チャーガ（和名カバノアナタケ）はシベリアや北海道を含め様々な地域での栽培が可能ですが、ロシア、バイカル地域の-40℃の肥沃な極寒地域で育つチャーガの効能は他と比べはるかに上回ります。</p>
+                <p class="section-belief__contents-text">広大なロシアの極寒を耐え抜き、自然の恵みを存分に蓄えたチャーガは、その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれております。</p>
+                <p class="section-belief__contents-text">私たちは、そんなロシア産一等級チャーガを皆様の健康と人生に寄り添うパートナーとして自信を持ってお届けします。</p>
+              </div>
+            </div>
+          </div>
+          <div class="accordion js-accordion u-hidden-o-md">
+            <div class="accordion__item js-accordion-trigger">
+              <div class="accordion__title-box">
+                <h3 class="accordion__title section-belief__contents-title">02 安心安全の品質管理</h3>
+              </div>
+              <div class="accordion__content"><img src="/img/top/img-belief-accordion-2.jpg" alt="">
+                <p class="section-belief__contents-text">毎日お口に入れるものだからこそ、安心安全の「本物」でなければなりません。</p>
+                <p class="section-belief__contents-text">紫翠チャーガは私たちが実際に現地へ足を運び、どんな環境で栽培されているのか、どのように生産されているのか、さらに現地の専門製造者がどんな人物なのかを知り、安全性や品質を直接確かめたうえで輸入販売しております。</p>
+              </div>
+            </div>
+          </div>
+          <ul class="section-belief__contents-title-box u-hidden-u-lg">
+            <li>
+              <h3 class="section-belief__contents-title js-belief-toggle is-active" id="belief-0"><span>01</span>ロシア産最高級チャーガ</h3>
+            </li>
+            <li>
+              <h3 class="section-belief__contents-title js-belief-toggle" id="belief-1"><span>02</span>安心安全の品質管理</h3>
+            </li>
+          </ul>
+          <div class="section-belief__contents-text-box fadeOn u-hidden-u-lg" id="js-belief-description"><img src="/img/top/img-belief-accordion-1.jpg" alt="">
+            <p class="section-belief__contents-text">チャーガ（和名カバノアナタケ）はシベリアや北海道を含め様々な地域での栽培が可能ですが、<br>ロシア、バイカル地域の-40℃の肥沃な極寒地域で育つチャーガの効能は他と比べはるかに上回ります。</p>
+            <p class="section-belief__contents-text">広大なロシアの極寒を耐え抜き、自然の恵みを存分に蓄えたチャーガは、その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれております。</p>
+            <p class="section-belief__contents-text">私たちは、そんなロシア産一等級チャーガを<br>皆様の健康と人生に寄り添うパートナーとして自信を持ってお届けします。</p>
+          </div>
+        </div>
+      </section>
+      <div class="package-box"></div>
+      <section class="section-news">
+        <h2 class="section-news__title">お知らせ</h2>
+        <div class="section-news__wrapper">
+          <?php
+          $wp_query = new WP_Query();
+          $my_posts = array(
+            'post_type' => 'post',
+            'post__not_in' => get_option('sticky_posts'),
+            'posts_per_page' => '3',
+            'category_name' => 'news',
+            // 'cat' => 4,//どちらでもフィルターができる
+          );
+          $wp_query->query($my_posts);
+          if ($wp_query->have_posts()) : while ($wp_query->have_posts()) : $wp_query->the_post();
+              // タイトル
+              $title = wp_trim_words(get_the_title(), 50, '...');
+              // タイトル
+              //記事の作成日
+              $date = get_the_date('Y-n-j');
+              //記事のリンク
+              $link = get_permalink();
+          ?>
+              <div class="news-card">
+                <div class="news-card__img"><img src=<?php echo the_post_thumbnail_url(); ?> alt=""></div>
+                <p class="news-card__date"><?php echo $date; ?></p>
+                <p class="news-card__title"><?php echo $title; ?></p>
+              </div>
 
-<main id="primary" class="site-main">
-  <!-- action button -->
-  <div class="actionBtn action_bottom_modal_btn__left">
-    <p class="action_bottom_modal_btn__ttl">aeainoestarsitne</p>
-    <a href="" class="action_bottom_modal_btn__txt">022-215-2989</a>
-    <a href="" class="action_bottom_modal_btn__txt net">iaiotatietan</a>
-  </div>
-  <div class="actionBtn action_bottom_modal_btn__right">
-    <p class="action_bottom_modal_btn__ttl">東口店のご予約</p>
-    <a href="" class="action_bottom_modal_btn__txt">022-215-2989</a>
-    <a href="" class="action_bottom_modal_btn__txt net">ネット予約</a>
-  </div>
-  <!-- main visual -->
-  <div class="main-visual">
-    <div class="main-visual__inner">
-      <p class="main-visual__inner--txt">lead title<br>lead text</p>
-    </div>
-  </div>
-  <!-- menu section -->
-  <div id="section1" class="menu-section">
-    <div class="menu-wrapper">
-      <p class="menu-wrapper__caution txt_sm sp_none">新鮮なお肉を椎入れておりますので<br>日によって取り揃えている部位が異なります。</p>
-      <div class="menu-wrapper__items">
-        <div class="menu-wrapper__items--1">
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
+          <?php endwhile;
+          endif;
+          wp_reset_postdata(); ?>
+        </div><a class="btn-type1" href="/info">もっと見る
+          <div class="arrow"></div></a>
+      </section>
+      <section class="section-faq-link">
+        <div class="section-faq-link__inner">
+          <div class="section-faq-link__box">
+            <div class="section-faq-link__title">お問い合わせ</div>
+            <div class="section-faq-link__text">ご注文や商品に関するご相談は<br>お問い合わせフォームをご利用ください。</div><a class="btn-type1" href="/contact">お問い合わせ
+              <div class="arrow"></div></a>
+          </div>
+          <div class="section-faq-link__box">
+            <div class="section-faq-link__title">よくある質問</div>
+            <div class="section-faq-link__text">よくある質問をまとめましたので<br>ぜひご参考ください。</div><a class="btn-type1" href="/faq">よくある質問
+              <div class="arrow"></div></a>
+          </div>
         </div>
-        <div class="menu-wrapper__items--2">
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box sp_none">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">仙台牛カルビ</dt>
-            <dd class="menu-box__price menu_txt">一二八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上塩牛タン</dt>
-            <dd class="menu-box__price menu_txt">八八〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">上ハラミ</dt>
-            <dd class="menu-box__price menu_txt">一一〇〇<span>円</span></dd>
-          </dl>
-          <dl class="menu-box">
-            <dt class="menu-box__item menu_txt">冷麺</dt>
-            <dd class="menu-box__price menu_txt">九〇〇<span>円</span></dd>
-          </dl>
-        </div>
-      </div>
+      </section><a class="section-instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">
+        <p>Instagramでも各種商品の案内や情報を発信しています！</p><img src="/img/common/icon-instagram.svg" alt=""></a>
     </div>
-    <h2 class="menu-section__ttl sct_ttl_lg">お品書き</h2>
-  </div>
-  <a href="" class="btn-bg">メニューはこちら</a>
-  <!-- kodawari section -->
-  <div id="section2" class="kodawari-section section">
-    <div class="kodawari-section__img-box section__img-box">
-      <img src="/assets/images/section_img1.jpg" alt="">
-    </div>
-    <div class="kodawari-section__txt-box section__txt-box">
-      <div class="txt-box">
-        <h2 class="txt-box__ttl sct_ttl">仙台牛にこだわり<br>炭火で焼く</h2>
-        <p class="txt-box__txt">仙台牛にこだわる、30年引き継いだ味。仙台牛に 仙台牛にこだわる、30年引き継いだ味仙台牛仙台 仙台牛にこだわる、30仙台</p>
-        <a href="" class="txt-box__btn btn_color_change">仙台牛へのこだわり</a>
-      </div>
-    </div>
-  </div>
-  <!-- reservation section -->
-  <div id="section3" class="reservation-section section">
-    <div class="reservation-section__img-box section__img-box">
-      <img src="/assets/images/section_img2.png" alt="">
-    </div>
-    <div class="reservation-section__txt-box">
-      <div class="txt-box">
-        <h2 class="txt-box__ttl sct_ttl">仙台牛にこだわり<br>炭火で焼く</h2>
-        <p class="txt-box__txt">仙台牛にこだわる、30年引き継いだ味。仙台牛に 仙台牛にこだわる、30年引き継いだ味仙台牛仙台 仙台牛にこだわる、30仙台</p>
-        <a href="" class="txt-box__btn btn_color_change">ご予約はこちら</a>
-      </div>
-    </div>
-  </div>
-  <!-- info section -->
-  <div id="section4" class="info-section">
-    <div class="info-section__img-box"></div>
-    <div class="info-section__txt-box txt-box">
-      <h2 class="txt-box__ttl sct_ttl_lg pc">店舗案内</h2>
-      <div class="txt-box__contents">
-        <table class="">
-          <tr>
-            <th>住所</th>
-            <td>〒984-0805<br>仙台市<span class="space"></span>若林区<span class="space"></span>南材木町92</td>
-          </tr>
-          <tr>
-            <th>営業日</th>
-            <td>【火曜−土曜】<br class="sp"><span>17:00<span class="space"></span>–<span class="space"></span>25:00</span>
-              <br>【日曜−祝日】<br class="sp"><span>17:30<span class="space"></span>–<span class="space"></span>24:00</span>
-            </td>
-          </tr>
-          <tr>
-            <th>定休日</th>
-            <td>月曜日</td>
-          </tr>
-          <tr>
-            <th>電話</th>
-            <td><a class="tel" href="tel:0222152989">022-215-2989</a></td>
-          </tr>
-          <tr>
-            <th>最寄駅</th>
-            <td>地下鉄<span class="space"></span>河原町<span class="space"></span>駅</td>
-          </tr>
-          <tr>
-            <th>席数</th>
-            <td>36席</td>
-          </tr>
-          <tr>
-            <th>個室</th>
-            <td>無</td>
-          </tr>
-          <tr>
-            <th>貸切</th>
-            <td>不可</td>
-          </tr>
-          <tr>
-            <th>駐車場</th>
-            <td>3台<br>コインパーキング<span class="space sp-none"></span>有り</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-    <a href="" class="txt-box__btn btn_color_change">google map</a>
-  </div>
-</main><!-- #main -->
+
 <?php
-get_sidebar();
 get_footer();
