6d9792145af6165e1f22ffc545ae741c46f3fc5b
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-05 05:44:38 +0900

    [update]footer更新

--- footer.php
+++ footer.php
@@ -1,4 +1,32 @@
 <?php wp_footer(); ?>
+  <footer class="footer" id="js-footer">
+    <div class="footer__inner"><a class="js-scroll-to-id top-link" href="#"><img src="/img/common/icon-top-link.svg" alt=""></a><a class="footer__brand" href="/"><img src="/img/common/logo-white.png" alt=""></a>
+      <ul class="footer-nav">
+        <li class="footer-nav__item"><a href="/">トップ</a></li>
+        <li class="footer-nav__item"><a href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">購入ページ<img src="/img/common/outside-link-white.svg" alt=""></a></li>
+        <li class="footer-nav__item"><a href="/faq">よくある質問</a></li>
+        <li class="footer-nav__item"><a href="/contact">お問い合わせ</a></li>
+        <li class="footer-nav__item"><a href="/company">会社概要</a></li>
+        <li class="footer-nav__item"><a href="/pp">プライバシーポリシー</a></li>
+      </ul>
+      <div class="footer-contact">
+        <div class="footer-contact__block">
+          <p class="footer-contact__lead">お電話でのお問い合わせ</p>
+          <div class="footer-contact__link"><img class="is-tel" src="/img/common/icon-tel-white.svg" alt="">
+            <p>0120-197-013</p>
+          </div>
+          <p class="footer-contact__note">受付時間 9:00~17:00 (土日祝を除く)</p>
+        </div>
+        <div class="footer-contact__block">
+          <p class="footer-contact__lead">メールでのお問い合わせ</p>
+          <div class="footer-contact__link"><img src="/img/common/icon-mail-white.svg" alt=""><a class="is-small" href="/contact">お問い合わせフォーム</a></div>
+        </div>
+      </div>
+      <p class="copyright">©︎2022 株式会社エストラスト</p>
+    </div>
+  </footer><script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 </body>
 
 </html>
