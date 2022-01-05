294e67bda5d9f770eb5bfb5a1d9522d613fa11d0
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-05 09:14:59 +0900

    [feat]会社概要の追加

---
+++ page-company.php
@@ -0,0 +1,33 @@
+<?php
+get_header();
+?>
+
+    <section class="mainvisual-lower2">
+      <div class="mainvisual-lower2__inner">
+        <h1 class="mainvisual-lower2__title">会社概要</h1>
+        <p class="mainvisual-lower2__sub-title">Company info</p>
+      </div>
+    </section>
+    <section class="section-company">
+      <div class="section-company__wrapper">
+        <dl class="section-company__info-list">
+          <dt class="section-company__info-title">会社名</dt>
+          <dd class="section-company__info-description">株式会社エストラスト</dd>
+          <dt class="section-company__info-title">設立</dt>
+          <dd class="section-company__info-description">1996年06月10日</dd>
+          <dt class="section-company__info-title">所在地</dt>
+          <dd class="section-company__info-description">〒060-0005<br>北海道札幌市中央区北5条西13丁目1-116 YN北五条ビル6F</dd>
+          <dt class="section-company__info-title">電話・FAX</dt>
+          <dd class="section-company__info-description">TEL：011-280-5220<br>FAX：011-206-9119</dd>
+          <dt class="section-company__info-title">フリーダイヤル</dt>
+          <dd class="section-company__info-description"><a href="">0120-197-013</a></dd>
+          <dt class="section-company__info-title">営業時間</dt>
+          <dd class="section-company__info-description">平日 9:00～17:00<span class="u-d-ib-xs">（祝日・年末年始を除く）</span></dd>
+        </dl>
+      </div><a class="btn-type1" href="/contact">お問い合わせ
+        <div class="arrow"></div></a><a class="btn-type1" href="/">トップページへ戻る
+        <div class="arrow"></div></a>
+    </section><a class="section-instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">
+      <p>Instagramでも各種商品の案内や情報を発信しています！</p><img src="/img/common/icon-instagram.svg" alt=""></a>
+<?php
+get_footer();
