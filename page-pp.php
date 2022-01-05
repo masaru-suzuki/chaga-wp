6e06388f9671af1025277c8fd67e2f6180e5a464
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-05 09:14:46 +0900

    [feat]プライバシーポリシーの追加

---
+++ page-pp.php
@@ -0,0 +1,73 @@
+<?php
+get_header();
+?>
+
+    <section class="mainvisual-lower2">
+      <div class="mainvisual-lower2__inner">
+        <h1 class="mainvisual-lower2__title">プライバシーポリシー</h1>
+        <p class="mainvisual-lower2__sub-title">privacy policy</p>
+      </div>
+    </section>
+    <div class="section-privacy-policy">
+      <div class="section-privacy-policy__wrapper">
+        <section class="section-privacy-policy__block">
+          <h2 class="section-privacy-policy__title">個人情報保護に関する基本的な方針</h2>
+          <p class="section-privacy-policy__lead">株式会社テック（以下｢当社｣）は、個人情報保護の重要性を十分認識し、個人情報の適正な取り扱いを行っていくことが、企業として重大な社会的責務と考えております。当社は、以下の基本的な方針に従い全社を挙げて個人情報の保護に努めてまいります。</p>
+          <ol class="section-privacy-policy__contents">
+            <li>1．当社は、個人情報の保護に関連する法令及び規範等を遵守いたします。</li>
+            <li>2．当社は、個人情報の収集目的を明確に定めるとともに、その目的の範囲内で収集します。また、その利用は、収集目的から逸脱しない範囲で取り扱います。</li>
+            <li>3．当社は、個人情報を取り扱う業務に従事する者に対して必要な教育研修等を実施するとともに適切な監督を行います。また、個人情報の取り扱いを外部に委託する場合には、守秘義務契約の締結等により委託先においても適正に取り扱われるよう管理、監督します。</li>
+            <li>4．当社は、個人情報の保護の一層の推進のため、社内規定、管理体制、安全管理措置、その他必要な措置の継続的な改善をいたします。</li>
+            <li>5．当社は、個人情報の不正アクセス、紛失、破壊、改ざん、漏洩等のトラブルを引き起こさないように安全対策を講じ、予防並びに是正をいたします。</li>
+          </ol>
+        </section>
+        <section class="section-privacy-policy__block">
+          <h2 class="section-privacy-policy__title">個人情報の利用目的について</h2>
+          <p class="section-privacy-policy__lead">当社は、個人情報を以下の基本的な方針に従い利用させていただきます。</p>
+          <ol class="section-privacy-policy__contents">
+            <li>1．お客様への商品の発送および代金の請求のためにお客様の氏名、住所、電話番号などの連絡先情報を利用します。また、代金の請求に関連してご指定いただいたクレジットカード番号、銀行口座などのお支払情報を利用する場合があります。</li>
+            <li>2．ご注文の内容や配送方法などを連絡したり確認するために、お客様の氏名、住所、メールアドレスなどの連絡先情報、ご注文いただいた商品の種類や数量、ご請求金額などの情報を利用します。</li>
+            <li>3．お客様に当社の商品・サービスのご案内をするために、ご利用された履歴や、お客様の氏名、住所、メールアドレスなどの連絡先情報を利用します。</li>
+            <li>4．当社の商品・サービスの改善を行うために、お客様から寄せられたご意見やアンケートの結果、ご利用履歴などを利用します。</li>
+            <li>5．お客様からのご要望、お問い合わせに対する回答をするために、お客様の氏名、住所、メールアドレスなどの連絡先情報を利用します。</li>
+          </ol>
+        </section>
+        <section class="section-privacy-policy__block">
+          <h2 class="section-privacy-policy__title">個人情報の管理について</h2>
+          <p class="section-privacy-policy__lead">当社は以下の方針で個人情報を管理します。</p>
+          <ol class="section-privacy-policy__contents">
+            <li>1．個人情報保護法やガイドラインに従って必要な社内体制を整備し、従業員から個人情報の取扱を適正に行う旨の誓約書を取得します。</li>
+            <li>2．個人情報の利用を業務上必要な社員だけに制限し、個人情報が含まれる媒体などの保管・管理などに関する規則を作り、個人情報保護のための予防措置を講じます。</li>
+            <li>3．システムに保存されている個人情報については、業務上必要な社員だけが利用できるようアカウントとパスワードを用意し、アクセス権限管理を実施します。なお、アカウントとパスワードは漏えい、滅失のないよう厳重に管理します。</li>
+            <li>4．サービスに支障が生じないことを前提として、個人情報の受領時から一定期間経過後、個人情報は随時削除していきます。</li>
+          </ol>
+        </section>
+        <section class="section-privacy-policy__block">
+          <h2 class="section-privacy-policy__title">第三者の提供について</h2>
+          <p class="section-privacy-policy__lead">当社は以下の方針で個人情報を管理します。</p>
+          <ol class="section-privacy-policy__contents">
+            <li>1．当社は、あらかじめ本人の許諾なく、第三者に個人情報は提供しません。ただし、保護法その他法令に別異の定めがある場合は除きます。</li>
+            <li>2．尚、商品の配送等、お客様との取引を遂行するにあたり、運送業者等の必要な外部委託を行う場合は第三者提供にはあたりません。</li>
+            <li>3．ご注文の際には、このプライバシーポリシーを理解のうえ、お客様の認識・許可のうえ、ご注文いただくことになります。</li>
+          </ol>
+        </section>
+        <section class="section-privacy-policy__block">
+          <h2 class="section-privacy-policy__title">個人情報の開示について</h2>
+          <p class="section-privacy-policy__lead">当社は以下の方針で個人情報を管理します。</p>
+          <ol class="section-privacy-policy__contents">
+            <li>1．当社は、お客様の同意がない限り、個人情報を第三者に開示することはありません。ただし、以下の事例に該当する場合は除きます。</li>
+            <li class="dot">法令に基づき裁判所や警察等の公的機関から要請があった場合</li>
+            <li class="dot">法令に特別の規定がある場合</li>
+            <li class="dot">お客様や第三者の生命・身体・財産を損なうおそれがあり、本人の同意を得ることが出来無い場合</li>
+            <li class="dot">法令や当社のご利用規約・注意事項に反する行動から、当社の権利、財産またはサービスを保護または防禦する必要があり、本人の同意を得ることができない場合</li>
+            <li>2．お客様から個人情報の開示要求があった場合は、本人であることが確認できた場合に限り開示します。</li>
+            <li class="space">【ご本人確認の方法】<br>ご本人であることが証明出来る物（免許証や保険証など）の写しを、当社宛に郵送下さい。内容を確認させて頂き、本人であることが確認出来次第、開示させて頂きます。</li>
+          </ol>
+        </section>
+      </div><a class="btn-type1" href="/contact">お問い合わせ
+        <div class="arrow"></div></a><a class="btn-type1" href="/">トップページへ戻る
+        <div class="arrow"></div></a>
+    </div><a class="section-instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">
+      <p>Instagramでも各種商品の案内や情報を発信しています！</p><img src="/img/common/icon-instagram.svg" alt=""></a>
+<?php
+get_footer();
