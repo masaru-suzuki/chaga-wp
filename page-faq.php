b11f8938d9b094ffcdc720d4730074afa970b454
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-05 09:05:29 +0900

    [feat]faqページの追加

---
+++ page-faq.php
@@ -0,0 +1,261 @@
+<?php
+get_header();
+?>
+
+    <section class="mainvisual-lower">
+      <div class="mainvisual-lower__inner">
+        <h1 class="mainvisual-lower__title">よくある質問</h1>
+        <p class="mainvisual-lower__sub-title">FAQ</p>
+      </div>
+    </section>
+    <section class="section-faq">
+      <p class="section-faq__lead">よくある質問をまとめましたのでぜひご参考ください。</p>
+      <div class="section-faq__wrapper">
+        <ul class="accordion js-accordion section-faq__list">
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">エキスの中にかたまりのようなものがあるのですが。</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">エキスの養分が沈殿し固まったものです。飲用前に軽く振っていただけると軽減されます。<br>品質にはまったく問題はございませんのでご安心下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">副作用の心配はありますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">食品ですので基本的に副作用の心配はございませんが好転反応として稀に湿疹や体のだるさを感じる方がいらっしゃいます。<br>何日が飲み続けていただくと治まりますが、尿が近くなったり、食物繊維が豊富な為便が緩くなることがございます。<br>気になるようでしたら量、回数を減らすなどして調整して下さい。また、チャーガは白樺に寄生するきのこですのできのこアレルギーや白樺アレルギーの方はお医者様へご相談下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">ペットに飲ませても大丈夫ですか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">大丈夫です。ご愛飲いただいているお客様の中にもペットも飲んでいるという方がいらっしゃいます。<br>ただし、尿が近くなったり便が緩くなるという症状もございますので、人と同量ではなく薄めるなどして量を調整していただいた方がよろしいと思います。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">どのような味がしますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">少し苦味や酸味を感じる方もいらっしゃるかと思われますが、水（またはぬるま湯）で薄めて飲用していただきますので後味も残らず飲みやすくなっております。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">妊娠中・授乳中でも飲用出来ますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">大丈夫です。気になる方は、お医者様にご相談の上お飲み下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">薬と併用しても大丈夫でしょうか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">一般の食品と同じように考えていただいて結構です。食事制限が必要な方や気になる方はかかりつけのお医者様へご相談下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">チャーガの摂取上限量はありますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">摂取上限量はありませんが、適量を継続して毎日取り入れていただくとよいでしょう。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">どのくらいの濃さで飲めば良いのですか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">200cc～300ccほどの水、またはぬるま湯でお飲みいただくのが宜しいと思います。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">どれくらい保存がきくのでしょうか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">外箱に賞味期限がございますのでご確認下さい。開封したものは賞味期限に関わらずお早めにお飲み下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">冷たいほうがいいの？温かいほうがいいの？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">お好みでお飲み下さい。（チャーガに含まれる多糖類が減少するので60℃以上の高温はお避け下さい。）</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">子供や高齢者でも飲めますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">大丈夫です。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">糖尿病を患っているのですがどう飲めばいいでしょうか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">数値の気になる方は朝晩2回をお勧め致します。体調が安定してきましたら量を減らすなど調整してみて下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">保管方法を教えてください。</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">未開封のものは常温で保管いただいて大丈夫です。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">一日のうちどのタイミングで飲めばいいですか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">起床時やお食事前などの空腹時がお勧めです。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">実感するのにどれくらいかかるの？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">早い方は２～3日程で体感される方もいらっしゃるようですが、体調にもよりますので申し訳ございませんがお答えしかねます。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">紫翠チャーガはどんな人にお勧めですか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">チャーガにはガン細胞抑制作用や抗アレルギー作用、血糖値低下作用などの有効性を示す研究結果も多数報告されておりますので、皆さまの健康維持、病気の予防などにもお役立ていただけると思っております。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">注文後の変更、キャンセルは出来ますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">発送前でしたら可能です。変更・キャンセルがございましたら、まずはお電話でご相談下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">オンラインショップ以外でも注文できますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">お電話でもご注文をお受け致しております。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">定期購入したいのですがどうしたらいいでしょうか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">商品一覧の定期購入よりお申し込み下さい。ご希望の個数とお日にちに合わせて発送致します。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">注文後どれくらいで届きますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">ご注文確認後1日以内(平日)に発送手配を致します。送り先によっては発送後３～4日かかる場合がございます。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">返品はできますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">基本的には未開封のものに限らせていただいております。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">贈答用で購入したいのですがラッピングしてもらえますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">商品一覧より「ギフト用包装」をお申し込み下さい。</p>
+              </div>
+            </div>
+          </li>
+          <li class="accordion__item js-accordion-trigger section-faq__item">
+            <div class="section-faq__title-box"><span class="section-faq__icon-question">Q</span>
+              <h2 class="section-faq__title">支払いはどんな方法が選べますか？</h2><img class="section-faq__open-btn" src="/img/common/icon-circle-arrow-gray.svg" alt="">
+            </div>
+            <div class="accordion__content">
+              <div class="section-faq__text-box"><span class="section-faq__icon-answer">A</span>
+                <p class="section-faq__text">振込（ご入金確認後に発送）と代金引換が選べます。</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div><a class="btn-type1" href="/">トップページへ戻る
+        <div class="arrow"></div></a><a class="btn-type1 is-outside-link" href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">オンラインショップへ
+        <div class="arrow"></div></a>
+    </section>
+    <section class="section-faq-link is-faq">
+      <div class="section-faq-link__inner">
+        <div class="section-faq-link__box">
+          <div class="section-faq-link__title">お問い合わせ</div>
+          <div class="section-faq-link__text">ご注文や商品に関するご相談は<br>お問い合わせフォームをご利用ください。</div><a class="btn-type1" href="/contact">お問い合わせ
+            <div class="arrow"></div></a>
+        </div>
+      </div>
+    </section><a class="section-instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">
+      <p>Instagramでも各種商品の案内や情報を発信しています！</p><img src="/img/common/icon-instagram.svg" alt=""></a>
+<?php
+get_footer();
