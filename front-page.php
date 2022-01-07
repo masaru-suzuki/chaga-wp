d6fb1df9879bf6de210f77550c58ba3cfffd8b74
masaru-suzuki <m.suzuki@tam-tam.co.jp>
2022-01-07 21:07:28 +0900

    [update]画像ファイルとJSファイルを更新

--- front-page.php
+++ front-page.php
@@ -1,17 +1,21 @@
 <?php
 get_header();
 ?>
-    <section class="main-visual" id="js-main-visual"><img class="main-visual__logo" src="<?php echo get_template_directory_uri(); ?>/img/common/logo.png" alt="">
+<section class="main-visual" id="js-main-visual"><img class="main-visual__logo" src="<?php echo get_template_directory_uri(); ?>/img/common/logo.png" alt="">
       <div class="main-visual__text-box">
         <h2 class="main-visual__title">チャーガとは</h2>
         <p class="main-visual__sub-title">About chaga</p>
         <p class="main-visual__lead">和名はカバノアナタケ。<br>｢森の宝石｣とも呼ばれる<br>白樺の幹に寄生するキノコのことです。</p>
         <div class="main-visual__img-box u-hidden-o-md"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-mainvisual.jpg" alt=""></div>
-        <p class="main-visual__text">導入テキストが入ります。和名はカバノアナタケ。｢森の宝石｣とも呼ばれる白樺の幹に寄生するキノコのことです。導入テキストが入ります。和名はカバノアナタケ。｢森の宝石｣とも呼ばれる白樺の幹に寄生するキノコのことです。導入テキストが入ります。</p><a class="btn-type1" href="/chaga">チャーガについて
+        <p class="main-visual__text">森のダイヤモンドと呼ばるチャーガ（カバノアナタケ）は白樺の幹にに寄生するキノコの一種です。<br>チャーガには強い抗酸化作用を持つフラボノイドやΒグルカン（多糖類）をはじめとする栄養素が豊富に含まれ、体の酸化と糖化を防ぎ免疫力の向上に役立てられます。</p><a class="btn-type1" href="/chaga">チャーガについて
           <div class="arrow"></div></a>
       </div>
     </section>
+
+
     <div class="fixed-link" id="js-fixed-link"><a class="fixed-link__shopify" href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">購入する</a><a class="fixed-link__instagram" href="https://www.instagram.com/shisuichaga/" target="_blank">Follow us<img src="<?php echo get_template_directory_uri(); ?>/img/common/icon-instagram.svg" alt=""></a></div>
+
+
     <div class="contents-wrapper" id="js-contents-wrapper">
       <div class="item-slider">
         <div class="item-slider__item"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-slider1.jpg" alt=""></div>
@@ -22,6 +26,9 @@ get_header();
         <div class="item-slider__item"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-slider3.jpg" alt=""></div>
       </div><a class="btn-type1 u-mt-60-xs u-mt-80-lg" href="https://shisui-chaga.myshopify.com/collections/all/" target="_blank">商品一覧へ
         <div class="arrow"></div></a>
+
+
+
       <section class="section-efficacy">
         <h2 class="section-efficacy__title">チャーガに含まれる豊富な栄養成分が、<br>あなたの日々の健康維持をサポートいたします。</h2>
         <div class="section-efficacy__contents">
@@ -29,7 +36,7 @@ get_header();
             <ul class="section-efficacy__ingredient">
               <li class="js-efficacy-toggle is-active" id="ingredient-0">B-グルカン</li>
               <li class="js-efficacy-toggle" id="ingredient-1">ポリフェノール</li>
-              <li class="js-efficacy-toggle" id="ingredient-2">マグネシウム</li>
+              <li class="js-efficacy-toggle" id="ingredient-2">メラニン</li>
               <li class="js-efficacy-toggle" id="ingredient-3">サポニン</li>
               <li class="js-efficacy-toggle" id="ingredient-4">エルゴステロール</li>
               <li class="js-efficacy-toggle" id="ingredient-5">ベツリン酸</li>
@@ -38,17 +45,18 @@ get_header();
               <li class="js-efficacy-toggle" id="ingredient-8">ナトリウム</li>
               <li class="js-efficacy-toggle" id="ingredient-9">フラボノイド</li>
               <li class="js-efficacy-toggle" id="ingredient-10">イノシトール</li>
-              <li class="js-efficacy-toggle" id="ingredient-11">リグニン</li>
-              <li class="js-efficacy-toggle u-hidden-u-lg" id="ingredient-12">メラニン</li>
+              <li class="js-efficacy-toggle" id="ingredient-11">食物繊維</li>
             </ul>
             <div class="section-efficacy__description">
               <div class="section-efficacy__description-next u-hidden-u-lg" id="js-efficacy-next-btn"></div>
               <div class="section-efficacy__description-prev u-hidden-u-lg" id="js-efficacy-prev-btn"></div>
-              <p id="js-efficacy-description">チャーガの栄養素の働きについてのテキストを入れられます。上記の各要素をタップするとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４るとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４るとテキストがそれぞれ入れ替わるようになりますテキストは４行まで入れられます４行まで入ります４行まで入ります４</p>
+              <p id="js-efficacy-description">β-グルカンとは、キノコなどに多く含まれている成分です。免疫力強化やコレステロール値上昇抑制などの効果があり、多くの可能性を秘めている成分として現在でも研究が進められています。</p>
             </div><img class="section-efficacy__img" src="<?php echo get_template_directory_uri(); ?>/img/top/img-efficacy.png" alt="">
           </div>
         </div>
       </section>
+
+
       <section class="section-belief">
         <div class="section-belief__lead">
           <div class="section-belief__lead-wrapper">
@@ -64,8 +72,8 @@ get_header();
                 <h3 class="accordion__title section-belief__contents-title">01 ロシア産最高級チャーガ</h3>
               </div>
               <div class="accordion__content"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-belief-accordion-1.jpg" alt="">
-                <p class="section-belief__contents-text">チャーガ（和名カバノアナタケ）はシベリアや北海道を含め様々な地域での栽培が可能ですが、ロシア、バイカル地域の-40℃の肥沃な極寒地域で育つチャーガの効能は他と比べはるかに上回ります。</p>
-                <p class="section-belief__contents-text">広大なロシアの極寒を耐え抜き、自然の恵みを存分に蓄えたチャーガは、その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれております。</p>
+                <p class="section-belief__contents-text">チャーガ（カバノアナタケ）はロシアや北半球の寒冷地での採取が可能ですが最高品質のチャーガはロシアのシベリア産と言われています。<br>良質なチャーガは森林の気温が－40℃以下の空気の澄んだ森林の豊かな土壌でなければいけません。そのような条件を満たす地域は現在ではシベリアのみと言われています。</p>
+                <p class="section-belief__contents-text">広大な紫水チャーガでは、厳しく豊潤なシベリアの大地の中でも最も良質なチャーガの発育条件を備えるバイカル湖地域での白樺に寄生しミネラル豊富なエキスを吸収し5年以上成育した最高品質のチャーガを原料として使用しています。ロシアの極寒を耐え抜き、自然の恵みを存分に蓄えたチャーガは、その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれております。</p>
                 <p class="section-belief__contents-text">私たちは、そんなロシア産一等級チャーガを皆様の健康と人生に寄り添うパートナーとして自信を持ってお届けします。</p>
               </div>
             </div>
@@ -76,7 +84,8 @@ get_header();
                 <h3 class="accordion__title section-belief__contents-title">02 安心安全の品質管理</h3>
               </div>
               <div class="accordion__content"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-belief-accordion-2.jpg" alt="">
-                <p class="section-belief__contents-text">毎日お口に入れるものだからこそ、安心安全の「本物」でなければなりません。</p>
+                <p class="section-belief__contents-text">毎日お口に入れるものだからこそ、<br>安心安全の「本物」でなければなりません。</p>
+                <p class="section-belief__contents-text">私たちが扱う紫翠チャーガは1950年代から病院で臨床実験を行い、チャーガを医薬品として活用した実績を持つロシアでISOの取得もしている信頼出来る工場で加工されたチャーガです。</p>
                 <p class="section-belief__contents-text">紫翠チャーガは私たちが実際に現地へ足を運び、どんな環境で栽培されているのか、どのように生産されているのか、さらに現地の専門製造者がどんな人物なのかを知り、安全性や品質を直接確かめたうえで輸入販売しております。</p>
               </div>
             </div>
@@ -90,12 +99,15 @@ get_header();
             </li>
           </ul>
           <div class="section-belief__contents-text-box fadeOn u-hidden-u-lg" id="js-belief-description"><img src="<?php echo get_template_directory_uri(); ?>/img/top/img-belief-accordion-1.jpg" alt="">
-            <p class="section-belief__contents-text">チャーガ（和名カバノアナタケ）はシベリアや北海道を含め様々な地域での栽培が可能ですが、<br>ロシア、バイカル地域の-40℃の肥沃な極寒地域で育つチャーガの効能は他と比べはるかに上回ります。</p>
-            <p class="section-belief__contents-text">広大なロシアの極寒を耐え抜き、自然の恵みを存分に蓄えたチャーガは、その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれております。</p>
-            <p class="section-belief__contents-text">私たちは、そんなロシア産一等級チャーガを<br>皆様の健康と人生に寄り添うパートナーとして自信を持ってお届けします。</p>
+            <p class="section-belief__contents-text">チャーガ（カバノアナタケ）はロシアや北半球の寒冷地での採取が可能ですが最高品質のチャーガはロシアのシベリア産と言われています。<br>良質なチャーガは森林の気温が－40℃以下の空気の澄んだ森林の豊かな土壌でなければいけません。そのような条件を満たす地域は現在ではシベリアのみと言われています。</p>
+            <p class="section-belief__contents-text">紫水チャーガでは、厳しく豊潤なシベリアの大地の中でも最も良質なチャーガの発育条件を備えるバイカル湖地域での白樺に寄生しミネラル豊富なエキスを吸収し5年以上成育した最高品質のチャーガを原料として使用しています。</p>
+            <p class="section-belief__contents-text">その希少価値と栄養成分の高さから「森のダイヤモンド」と呼ばれるチャーガを皆様の健康と人生に寄り添うパートナーとして自信を持ってお届けします。</p>
           </div>
         </div>
       </section>
+
+
+
       <div class="package-box"></div>
       <section class="section-news">
         <h2 class="section-news__title">お知らせ</h2>
